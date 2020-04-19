c27ee00775675892abdc41794c9037bc3cd97ae5
Tadej Novak <tadej@tano.si>
2020-04-19 11:24:06 +0200

    Do not include JS in special pages

--- pages/oauth2/error.tsx
+++ pages/oauth2/error.tsx
@@ -1,6 +1,10 @@
 import { NextPage } from 'next';
 import ErrorLayout from '../../layouts/ErrorLayout'
 
+export const config = {
+  unstable_runtimeJS: false,
+}
+
 const ErrorPage: NextPage = () => (
   <ErrorLayout title="403" text={'{{.Message}}'} />
 );
--- pages/oauth2/sign_in.tsx
+++ pages/oauth2/sign_in.tsx
@@ -1,9 +1,13 @@
 import { NextPage } from 'next';
 import ErrorLayout from '../../layouts/ErrorLayout'
 
+export const config = {
+  unstable_runtimeJS: false,
+}
+
 const ErrorPage: NextPage = () => (
   <ErrorLayout title="401" text="It appears you need to login to visit this page.">
-    <form method="GET" action="PROXYPREFIX/start" className="error-special">
+    <form method="GET" action="{{.ProxyPrefix}}/start" className="error-special">
       <input type="hidden" name="rd" value="{{.Redirect}}" />
       <button type="submit" className="button is-primary is-outlined">Sign in</button>
     </form>
