89666b4673e7c3c5281564bb1a4889cd7a59e219
Tadej Novak <tadej@tano.si>
2021-11-14 16:03:16 +0100

    Add ability to only show current year in the footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,31 +3,43 @@ import { FunctionComponent } from 'react'
 type Props = {
   extra?: string
   homeUrl?: string
+  onlyCurrentYear?: boolean
 }
 
-const Footer: FunctionComponent<Props> = ({ children, extra, homeUrl }) => (
-  <footer className="footer">
-    <div className="container">
-      <nav className="level">
-        <div className="level-left">
-          <div className="level-item has-special-links">
-            &copy; 2004-2021{' '}
-            <a
-              href={homeUrl ? homeUrl : 'https://tano.si'}
-              property="cc:attributionName"
-              rel="cc:attributionURL"
-            >
-              Tadej&nbsp;Novak
-            </a>
+const Footer: FunctionComponent<Props> = ({
+  children,
+  extra,
+  homeUrl,
+  onlyCurrentYear,
+}) => {
+  let year = '2004-2021'
+  if (typeof onlyCurrentYear !== undefined && onlyCurrentYear) {
+    year = '2021'
+  }
+  return (
+    <footer className="footer">
+      <div className="container">
+        <nav className="level">
+          <div className="level-left">
+            <div className="level-item has-special-links">
+              &copy; {year}{' '}
+              <a
+                href={homeUrl ? homeUrl : 'https://tano.si'}
+                property="cc:attributionName"
+                rel="cc:attributionURL"
+              >
+                Tadej Novak
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="level-right">
-          <div className="level-item has-special-links">{children}</div>
-        </div>
-      </nav>
-      {extra && <div>{extra}</div>}
-    </div>
-  </footer>
-)
+          <div className="level-right">
+            <div className="level-item has-special-links">{children}</div>
+          </div>
+        </nav>
+        {extra && <div>{extra}</div>}
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
