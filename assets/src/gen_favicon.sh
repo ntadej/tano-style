b15fe6eb0ac2fa1d96fd0048a94044d7add9a928
Tadej Novak <tadej@tano.si>
2023-05-22 21:12:34 +0200

    Minor tweaks to the branding scripts

--- assets/src/gen_favicon.sh
+++ assets/src/gen_favicon.sh
@@ -5,30 +5,27 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd )"
 # Apple
 apple=("76" "76@2x" "60@2x" "60@3x" "83.5@2x")
 sizes=(76 152 120 180 167)
-length=${#apple[@]}
 
-for (( i=0; i<${length}; i++ ));
+for (( i=0; i<${#apple[@]}; i++ ));
 do
-    inkscape -z -e $dir/favicon/apple_${apple[$i]}.png -w ${sizes[$i]} $dir/src/logo_square.svg
+    inkscape -o "$dir/favicon/apple_${apple[$i]}.png" -w "${sizes[$i]}" "$dir/src/logo_square.svg"
 done
 
 # Other
 sizes=(16 32 36 48 72 96 144 192)
-length=${#sizes[@]}
-for (( i=0; i<${length}; i++ ));
+for (( i=0; i<${#sizes[@]}; i++ ));
 do
-    inkscape -z -e $dir/favicon/${sizes[$i]}.png -w ${sizes[$i]} $dir/src/logo.svg
+    inkscape -o "$dir/favicon/${sizes[$i]}.png" -w "${sizes[$i]}" "$dir/src/logo.svg"
 done
 
 # ICO
-convert $dir/favicon/16.png $dir/favicon/32.png $dir/favicon/48.png $dir/favicon.ico
+convert "$dir/favicon/16.png" "$dir/favicon/32.png" "$dir/favicon/48.png" "$dir/favicon.ico"
 
 # MS
 ms=("70" "150" "310")
 sizes=("70" "150" "310")
-length=${#ms[@]}
-for (( i=0; i<${length}; i++ ));
+for (( i=0; i<${#ms[@]}; i++ ));
 do
-    inkscape -z -e $dir/favicon/mstile_${ms[$i]}.png -w ${sizes[$i]} $dir/src/logo_text.svg
+    inkscape -o "$dir/favicon/mstile_${ms[$i]}.png" -w "${sizes[$i]}" "$dir/src/logo_text.svg"
 done
-convert $dir/favicon/mstile_150.png -gravity center -background transparent -extent 310x150 $dir/favicon/mstile_310x150.png
+convert "$dir/favicon/mstile_150.png" -gravity center -background transparent -extent "310x150" "$dir/favicon/mstile_310x150.png"
--- assets/src/gen_logo.sh
+++ assets/src/gen_logo.sh
@@ -3,26 +3,22 @@
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && cd .. && pwd )"
 
 # Logo
-name=("normal" "normal@2x" "mobile" "mobile@2x")
-sizes=(60 120 40 80)
+name=("main" "main@2x")
+sizes=(36 72)
 
-length=${#name[@]}
-
-for (( i=0; i<${length}; i++ ));
+for (( i=0; i<${#name[@]}; i++ ));
 do
-    inkscape -z -e $dir/logo/${name[$i]}.png -a 7:7:67:67 -w ${sizes[$i]} $dir/src/logo.svg
+    inkscape -o "$dir/logo/${name[$i]}.png" --export-area 7:7:67:67 -w "${sizes[$i]}" "$dir/src/logo.svg"
 done
 
 # Logo small
 name=("small" "small@2x")
 sizes=(48 96)
 
-length=${#name[@]}
-
-for (( i=0; i<${length}; i++ ));
+for (( i=0; i<${#name[@]}; i++ ));
 do
-    inkscape -z -e $dir/logo/${name[$i]}.png -w ${sizes[$i]} $dir/src/logo_small.svg
+    inkscape -o "$dir/logo/${name[$i]}.png" -w "${sizes[$i]}" "$dir/src/logo_small.svg"
 done
 
 # Google Apps
-inkscape -z -e $dir/google/apps.png -w 320 $dir/src/google.svg
+inkscape -o "$dir/google/apps.png" -w 320 "$dir/src/google.svg"
