0cc19da2992cee8ffd7bdad19e09c8c43839582b
Tadej Novak <tadej@tano.si>
2025-01-05 13:22:12 +0100

    Fix default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import { RootLayout } from '@/components/layouts'
 import { rootMetadata } from '@/components/metadata'
 import { rootViewport } from '@/components/viewport'
 import { Metadata, Viewport } from 'next'
+import { ReactNode } from 'react'
 
 import '@/index.css'
 
-export default RootLayout
+export default function PageLayout({ children }: { children: ReactNode }) {
+  return <RootLayout>{children}</RootLayout>
+}
 
 export const metadata: Metadata = rootMetadata
 export const viewport: Viewport = rootViewport
