0efd629252bd81b70832a1bf1253e150ae4b33cd
Tadej Novak <tadej@tano.si>
2025-01-05 14:41:54 +0100

    Make buttons customiseable

--- components/buttons.tsx
+++ components/buttons.tsx
@@ -5,11 +5,15 @@ import { StyledLink } from './links'
 
 export function ActionButton({
   children,
+  text,
+  colors,
   type,
   disabled,
   onClick,
 }: {
   children: ReactNode
+  text?: string
+  colors?: string
   type?: ButtonHTMLAttributes<HTMLButtonElement>['type']
   disabled?: ButtonHTMLAttributes<HTMLButtonElement>['disabled']
   onClick?: ButtonHTMLAttributes<HTMLButtonElement>['onClick']
@@ -19,10 +23,11 @@ export function ActionButton({
       type={type}
       disabled={disabled}
       className={clsx(
-        'transform rounded-lg px-6 py-3 text-lg font-medium tracking-wide',
-        'bg-primary-700 text-zinc-50',
+        'transform rounded-lg px-6 py-3 tracking-wide',
+        text ? text : 'text-lg font-medium',
+        colors ? colors : 'bg-primary-700 text-zinc-50',
         disabled && 'opacity-50',
-        !disabled && 'hover:bg-primary-600 hover:text-zinc-100',
+        !colors && !disabled && 'hover:bg-primary-600 hover:text-zinc-100',
         'transition-colors duration-300',
       )}
       onClick={onClick}
@@ -35,12 +40,16 @@ export function ActionButton({
 export function LinkButton({
   children,
   href,
+  text,
+  colors,
   target,
   rel,
   linkType,
 }: {
   children: ReactNode
   href: string
+  text?: string
+  colors?: string
   target?: string
   rel?: string
   linkType?: LinkType
@@ -52,8 +61,11 @@ export function LinkButton({
       rel={rel}
       linkType={linkType}
       className={[
-        'transform rounded-lg px-6 py-3 text-lg font-medium tracking-wide',
-        'bg-primary-700 text-zinc-50 hover:bg-primary-600 hover:text-zinc-100',
+        'transform rounded-lg px-6 py-3 tracking-wide',
+        text ? text : 'text-lg font-medium',
+        colors
+          ? colors
+          : 'bg-primary-700 text-zinc-50 hover:bg-primary-600 hover:text-zinc-100',
         'transition-colors duration-300',
       ]}
     >
